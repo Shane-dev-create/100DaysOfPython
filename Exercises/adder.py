dfaa67b2b45c8e6cbad79bb0023b15399c76b6cf
‘Shane-dev-create’ <shn.shane@gmail.com>
2024-03-05 16:13:48 +0200

    refactoring code

--- Exercises/adder.py
+++ Exercises/adder.py
@@ -1,3 +1,6 @@
+# This is a simple addition function that returns a function that adds a number to another number.
+# The function should take a number and return a function that takes another number.
+
 def make_adder(n):
     def adder(x):
         return n + x
